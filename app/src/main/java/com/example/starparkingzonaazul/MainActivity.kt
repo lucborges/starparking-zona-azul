d57713bc110eece9e8bcc47d600e8b2071c53353
Lucas Borges <lucaspy07@gmail.com>
2022-03-24 18:17:05 -0300

    create a navbar and splash screen
    create a navbar with icons and splash screen

--- app/src/main/java/com/example/starparkingzonaazul/MainActivity.kt
+++ app/src/main/java/com/example/starparkingzonaazul/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_StarparkingZonaAzul)
+
         setContentView(R.layout.activity_main)
     }
 }
