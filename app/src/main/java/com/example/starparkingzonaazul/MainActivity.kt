d892e5e24fa726e11d684816fa98a30dd192fdcf
Lucas Borges <lucaspy07@gmail.com>
2022-04-05 22:34:17 -0300

    navigation on app
    add a navigation on app

--- app/src/main/java/com/example/starparkingzonaazul/MainActivity.kt
+++ app/src/main/java/com/example/starparkingzonaazul/MainActivity.kt
@@ -1,44 +1,34 @@
 package com.example.starparkingzonaazul
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.example.starparkingzonaazul.fragments.HomeFragment
-import com.example.starparkingzonaazul.fragments.IrregularityFragment
-import com.example.starparkingzonaazul.fragments.MapFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_StarparkingZonaAzul)
         setContentView(R.layout.activity_main)
 
-        val homeFragment=HomeFragment()
-        val irregularityFragment=IrregularityFragment()
-        val mapFragment=MapFragment()
-        setCurrentFragment(homeFragment)
+        val buttonMap:Button = findViewById(R.id.acessButtonMap)
+        val buttonSearch:Button = findViewById(R.id.acessButtonSearch)
+        val buttonIrregularity:Button = findViewById(R.id.acessButtonIrregularity)
 
-        findViewById<BottomNavigationView>(R.id.bottom_navigation)
-            .setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.home->setCurrentFragment(homeFragment)
-                R.id.irregularity->setCurrentFragment(irregularityFragment)
-                R.id.map->setCurrentFragment(mapFragment)
-
-            }
-            true
+        buttonMap.setOnClickListener {
+            val intentMap = Intent(this, MapActivity::class.java)
+            startActivity(intentMap)
         }
 
+        buttonSearch.setOnClickListener{
+            val intentSearch = Intent(this, SearchActivity::class.java)
+            startActivity(intentSearch)
+        }
+
+        buttonIrregularity.setOnClickListener{
+            val intentIrregularity = Intent(this, IrregularityActivity::class.java)
+            startActivity(intentIrregularity)
+        }
     }
-
-    private fun setCurrentFragment(fragment:Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_container,fragment)
-            commit()
-        }
-
-
-
-
 }
